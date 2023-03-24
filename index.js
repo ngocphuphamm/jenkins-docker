79a89272828e12c251a1695eaaf215f8b1c5f495
ngocphuphamm <ngocphupham682001@gmail.com>
2023-03-24 16:07:53 +0700

    chnagE

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 const {PORT} = require('./config');
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+  res.send('Hello Wosdasdasdasdrld!');
 });
 
 var server = app.listen(PORT, function () {
